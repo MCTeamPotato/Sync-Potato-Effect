ac394dea8484ac433e02d3273903ccf66392396e
Kasualix <670067575@qq.com>
2023-02-20 23:52:04 +0800

    fix

--- src/main/java/com/teampotato/spe/EventHandler.java
+++ src/main/java/com/teampotato/spe/EventHandler.java
@@ -2,6 +2,8 @@ package com.teampotato.spe;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,8 +14,15 @@ import java.util.ArrayList;
 public class EventHandler {
     @SubscribeEvent
     public static void changeDimension(EntityTravelToDimensionEvent event) {
-        if (event.getEntity() instanceof PlayerEntity) {
-            PlayerEntity player = (PlayerEntity) event.getEntity();
+        ResourceLocation dim = event.getDimension().getRegistryName();
+        if (event.getEntity() instanceof PlayerEntity) event.getEntity().addTag("spe_" + dim.getNamespace() + "_" + dim.getPath());
+    }
+
+    @SubscribeEvent
+    public static void tick(TickEvent.PlayerTickEvent event) {
+        PlayerEntity player = event.player;
+        ResourceLocation dim = player.world.getDimensionKey().getRegistryName();
+        if (player.removeTag("spe_" + dim.getNamespace() + "_" + dim.getPath())) {
             ArrayList<EffectInstance> effects = new ArrayList<>(player.getActivePotionEffects());
             for (int i = effects.size() - 1; i >= 0; i --) {
                 player.removePotionEffect(effects.get(i).getPotion());
